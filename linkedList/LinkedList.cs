27d19d5e3561b4bc15d4ce7f2a09ada304c0148c
root <afurkankurt@outlook.com>
2024-02-12 16:02:45 +0100

    initial

---
+++ linkedList/LinkedList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace linkedList
+{
+    public class LinkedList
+    {
+        private Node head;
+        private int size;
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.size = 0;
+
+        }
+
+        public void Add(object o)
+        {
+            this.head = new Node(o, this.head);
+            size++;
+        }
+
+        public void Remove()
+        {
+            if (this.head != null)
+            {
+                this.head = this.head.Next;
+                size--;
+            }
+
+        }
+
+        public int Size
+        {
+            get { return this.size; }
+        }
+
+        public object Get(int index)
+        {
+            Node current = this.head;
+            for (int i = 0; i < index; i++)
+                current = current.Next;
+
+            return current.Data;
+        }
+
+        public override string ToString()
+        {
+            String str = "";
+            for (int i = 0; i < size; i++)
+            {
+                str += i;
+                str += ": ";
+                str += this.Get(i);
+                str += "\n";
+            }
+            return str;
+        }
+
+        public bool Contains(object o)
+        {
+            for (int i = 0; i < size; i++)
+            {
+
+                if (o.Equals(Get(i)))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ linkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace linkedList
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(1);
+            list.Add(2);
+            list.Add("Hello World!");
+            list.Add(3);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine(list.Contains(3));
+            list.Remove();
+            Console.WriteLine(list.ToString());
+            Console.WriteLine(list.Contains(3));
+
+        }
+    }
+}
